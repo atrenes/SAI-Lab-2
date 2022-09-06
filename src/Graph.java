a773c69c68d3c13ef5aaacbe25eaa14af496b30d
atrenes <hanalainendm@gmail.com>
2022-09-06 21:08:07 +0300

    Added Graph class

---
+++ src/Graph.java
@@ -0,0 +1,35 @@
+import java.util.*;
+
+public class Graph {
+    private LinkedList<Integer> adjLists[];
+    private boolean visited[];
+
+    // Graph creation
+    Graph(int vertices) {
+        adjLists = new LinkedList[vertices];
+        visited = new boolean[vertices];
+
+        for (int i = 0; i < vertices; i++)
+            adjLists[i] = new LinkedList<Integer>();
+    }
+
+    // Add edges
+    void addEdge(int src, int dest) {
+        adjLists[src].add(dest);
+        adjLists[dest].add(src);
+    }
+
+    // DFS algorithm
+    void DFS(int vertex, int dest) {
+        visited[vertex] = true;
+        System.out.print(vertex + " ");
+
+        Iterator<Integer> ite = adjLists[vertex].listIterator();
+        while (ite.hasNext()) {
+            int adj = ite.next();
+            if (!visited[adj] && !visited[dest]) { //TODO
+                DFS(adj, dest);
+            }
+        }
+    }
+}
