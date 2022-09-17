5d484e661293b99df28a2a310cc5d0ef2078b5ac
atrenes <hanalainendm@gmail.com>
2022-09-17 19:29:39 +0300

    Added BFS

--- src/Graph.java
+++ src/Graph.java
@@ -3,9 +3,11 @@ import java.util.*;
 public class Graph {
     private LinkedList<Integer> adjLists[];
     private boolean visited[];
+    private final int V;
 
     // Graph creation
     Graph(int vertices) {
+        this.V = vertices;
         adjLists = new LinkedList[vertices];
         visited = new boolean[vertices];
 
@@ -13,23 +15,117 @@ public class Graph {
             adjLists[i] = new LinkedList<Integer>();
     }
 
-    // Add edges
-    void addEdge(int src, int dest) {
-        adjLists[src].add(dest);
-        adjLists[dest].add(src);
+    void addEdge(String src, String dest) {
+        adjLists[stringToInt(src)].add(stringToInt(dest));
+        adjLists[stringToInt(dest)].add(stringToInt(src));
     }
 
     // DFS algorithm
-    void DFS(int vertex, int dest) {
+    void DFS(String start, String finish) {
+        int vertex = stringToInt(start);
+        int dest = stringToInt(finish);
         visited[vertex] = true;
-        System.out.print(vertex + " ");
+        System.out.print(" -> " + intToString(vertex));
 
         Iterator<Integer> ite = adjLists[vertex].listIterator();
         while (ite.hasNext()) {
             int adj = ite.next();
-            if (!visited[adj] && !visited[dest]) { //TODO
-                DFS(adj, dest);
+            if (!visited[adj] && !visited[dest]) {
+                DFS(intToString(adj), intToString(dest));
             }
         }
+    }
+
+    void BFS(String start, String finish) {
+        int s = stringToInt(start);
+        boolean visited[] = new boolean[V];
+
+        LinkedList<Integer> queue = new LinkedList();
+
+        visited[s] = true;
+        queue.add(s);
+
+        while (queue.size() != 0) {
+            s = queue.poll();
+            System.out.print(" -> " + intToString(s));
+            if (intToString(s).equals(finish)) return; //STOP SEARCHING WHEN WE REACH DEST
+
+            Iterator<Integer> i = adjLists[s].listIterator();
+            while (i.hasNext()) {
+                int n = i.next();
+                if (!visited[n]) {
+                    visited[n] = true;
+                    queue.add(n);
+                }
+            }
+        }
+    }
+
+    private int stringToInt(String s) {
+        return switch (s) {
+            case "Вильнюс" -> 0;
+            case "Брест" -> 1;
+            case "Витебск" -> 2;
+            case "Воронеж" -> 3;
+            case "Волгоград" -> 4;
+            case "Ниж.Новгород" -> 5;
+            case "Даугавпилс" -> 6;
+            case "Калининград" -> 7;
+            case "Каунас" -> 8;
+            case "Киев" -> 9;
+            case "Житомир" -> 10;
+            case "Донецк" -> 11;
+            case "Кишинев" -> 12;
+            case "С.Петербург" -> 13;
+            case "Рига" -> 14;
+            case "Москва" -> 15;
+            case "Казань" -> 16;
+            case "Минск" -> 17;
+            case "Мурманск" -> 18;
+            case "Орел" -> 19;
+            case "Одесса" -> 20;
+            case "Таллинн" -> 21;
+            case "Харьков" -> 22;
+            case "Симферополь" -> 23;
+            case "Ярославль" -> 24;
+            case "Уфа" -> 25;
+            case "Самара" -> 26;
+
+            default -> -1;
+        };
+    }
+
+    private String intToString(int index) {
+        return switch (index) {
+            case 0 -> "Вильнюс";
+            case 1 -> "Брест";
+            case 2 -> "Витебск";
+            case 3 -> "Воронеж";
+            case 4 -> "Волгоград";
+            case 5 -> "Ниж.Новгород";
+            case 6 -> "Даугавпилс";
+            case 7 -> "Калининград";
+            case 8 -> "Каунас";
+            case 9 -> "Киев";
+            case 10 -> "Житомир";
+            case 11 -> "Донецк";
+            case 12 -> "Кишинев";
+            case 13 -> "С.Петербург";
+            case 14 -> "Рига";
+            case 15 -> "Москва";
+            case 16 -> "Казань";
+            case 17 -> "Минск";
+            case 18 -> "Мурманск";
+            case 19 -> "Орел";
+            case 20 -> "Одесса";
+            case 21 -> "Таллинн";
+            case 22 -> "Харьков";
+            case 23 -> "Симферополь";
+            case 24 -> "Ярославль";
+            case 25 -> "Уфа";
+            case 26 -> "Самара";
+
+            default -> null;
+        };
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -3,12 +3,54 @@ import java.util.Map;
 
 public class Main {
     public static void main(String[] args) {
-        Graph g = new Graph(5);
-        g.addEdge(0,1);
-        g.addEdge(0,2);
-        g.addEdge(0,3);
-        g.addEdge(1,2);
-        g.addEdge(2,4);
-        g.DFS(0,3);
+        final int CITY_NUM = 27;
+        Graph g = new Graph(CITY_NUM);
+
+        g.addEdge("Вильнюс","Брест");
+        g.addEdge("Витебск","Брест");
+        g.addEdge("Витебск","Вильнюс");
+        g.addEdge("Воронеж","Витебск");
+        g.addEdge("Воронеж","Волгоград");
+        g.addEdge("Волгоград","Витебск");
+        g.addEdge("Витебск","Ниж.Новгород");
+        g.addEdge("Вильнюс","Даугавпилс");
+        g.addEdge("Калининград","Брест");
+        g.addEdge("Калининград","Вильнюс");
+        g.addEdge("Каунас","Вильнюс");
+        g.addEdge("Киев","Вильнюс");
+        g.addEdge("Киев","Житомир");
+        g.addEdge("Житомир","Донецк");
+        g.addEdge("Житомир","Волгоград");
+        g.addEdge("Кишинев","Киев");
+        g.addEdge("Кишинев","Донецк");
+        g.addEdge("С.Петербург","Витебск");
+        g.addEdge("С.Петербург","Калининград");
+        g.addEdge("С.Петербург","Рига");
+        g.addEdge("Москва","Казань");
+        g.addEdge("Москва","Ниж.Новгород");
+        g.addEdge("Москва","Минск");
+        g.addEdge("Москва","Донецк");
+        g.addEdge("Москва","С.Петербург");
+        g.addEdge("Мурманск","С.Петербург");
+        g.addEdge("Мурманск","Минск");
+        g.addEdge("Орел","Витебск");
+        g.addEdge("Орел","Донецк");
+        g.addEdge("Орел","Москва");
+        g.addEdge("Одесса","Киев");
+        g.addEdge("Рига","Каунас");
+        g.addEdge("Таллинн","Рига");
+        g.addEdge("Харьков","Киев");
+        g.addEdge("Харьков","Симферополь");
+        g.addEdge("Ярославль","Воронеж");
+        g.addEdge("Ярославль","Минск");
+        g.addEdge("Уфа","Казань");
+        g.addEdge("Уфа","Самара");
+
+        System.out.println("DFS: ");
+        g.DFS("Харьков", "Ниж.Новгород");
+        System.out.println();
+
+        System.out.println("BFS: ");
+        g.BFS("Харьков", "Ниж.Новгород");
     }
 }
